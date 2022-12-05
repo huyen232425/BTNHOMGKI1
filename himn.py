f9a70a52ba921ae1eb50b7b1fc760be8e03bd484
huyen232425 <119513548+huyen232425@users.noreply.github.com>
2022-12-05 10:39:58 +0700

    Create himn.py

---
+++ himn.py
@@ -0,0 +1,209 @@
+
+
+def lichthidaubangA():
+    print('________________Lịch thi đấu bảng A________________')
+    print('_Quatar & Ecuador>>>\nTime: 23:00\nDate: 20/11/2022')
+    print('_Senegal & Hà Lan>>>\nTime: 23:00\nDate: 21/11/2022')
+    print('_Quatar & Senegal>>>\nTime: 20:00\nDate: 25/11/2022')
+    print('_Hà Lan & Ecuador>>>\nTime: 23:00\nDate: 25/11/2022')
+    print('_Hà Lan & Quatar>>>\nTime: 22:00\nDate: 29/11/2022')
+    print('_Ecuador & Senegal>>>\nTime: 22:00\nDate: 29/11/2022')
+    print('___________________________________________________')
+
+def lichthidaubangB():
+    print('________________Lịch thi đấu bảng B________________')
+    print('_Anh & Iran>>>\nTime: 20:00\nDate: 21/11/2022')
+    print('_Mỹ & Xứ Wales>>>\nTime: 02:00\nDate: 22/11/2022')
+    print('_Xứ Wales & Iran>>>\nTime: 17:00\nDate: 25/11/2022')
+    print('_Anh & Mỹ>>>\nTime: 02:00\nDate: 26/11/2022')
+    print('_Xứ Wales & Anh>>>\nTime: 02:00\nDate: 30/11/2022')
+    print('_Iran & Mỹ>>>\nTime: 02:00\nDate: 30/11/2022')
+    print('___________________________________________________')
+
+def lichthidaubangC():
+    print('________________Lịch thi đấu bảng C________________')
+    print('_Argentina & Saudi Arabia>>>\nTime: 17:00\nDate: 22/11/2022')
+    print('_Mexico & Ba Lan>>>\nTime: 23:00\nDate: 22/11/2022')
+    print('_Ba Lan & Saudi Arabia>>>\nTime: 20:00\nDate: 26/11/2022')
+    print('_Argentina & Mexico>>>\nTime: 02:00\nDate: 27/11/2022')
+    print('_Ba Lan & Argentina>>>\nTime: 02:00\nDate: 01/12/2022')
+    print('_Saudi Arabia & Mexico>>>\nTime: 02:00\nDate: 01/12/2022')
+    print('___________________________________________________')
+
+def lichthidaubangD():
+    print('________________Lịch thi đấu bảng D________________')
+    print('_Đan Mạch & Tunisia>>>\nTime: 20:00\nDate: 22/11/2022')
+    print('_Pháp & Australia>>>\nTime: 02:00\nDate: 23/11/2022')
+    print('_Tunisia & Australia>>>\nTime: 17:00\nDate: 26/11/2022')
+    print('_Pháp & Đan Mạch>>>\nTime: 23:00\nDate: 26/11/2022')
+    print('_Tunisia & Pháp>>>\nTime: 22:00\nDate: 30/11/2022')
+    print('_Australia & Đan Mạch>>>\nTime: 22:00\nDate: 30/11/2022')
+    print('___________________________________________________')
+
+def lichthidaubangE():
+    print('________________Lịch thi đấu bảng E________________')
+    print('_Đức & Nhật Bản>>>\nTime: 20:00\nDate: 23/11/2022')
+    print('_Tây Ban Nha & Costa Rica>>>\nTime: 23:00\nDate: 23/11/2022')
+    print('_Nhật Bản & Costa Rica>>>\nTime: 17:00\nDate: 27/11/2022')
+    print('_Tây Ban Nha & Đức>>>\nTime: 02:00\nDate: 28/11/2022')
+    print('_Nhật Bản & Tây Ban Nha>>>\nTime: 02:00\nDate: 02/12/2022')
+    print('_Costa Rica & Đức>>>\nTime: 02:00\nDate: 02/12/2022')
+    print('___________________________________________________')
+
+def lichthidaubangF():
+    print('________________Lịch thi đấu bảng F________________')
+    print('_Ma-rốc & Croatia>>>\nTime: 17:00\nDate: 23/11/2022')
+    print('_Bỉ & Canada>>>\nTime: 02:00\nDate: 24/11/2022')
+    print('_Bỉ & Ma-rốc>>>\nTime: 20:00\nDate: 27/11/2022')
+    print('_Croatia & Canada>>>\nTime: 23:00\nDate: 27/11/2022')
+    print('_Croatia & Bỉ>>>\nTime: 22:00\nDate: 01/12/2022')
+    print('_Canada & Ma-rốc>>>\nTime: 22:00\nDate: 01/12/2022')
+    print('___________________________________________________')
+
+def lichthidaubangG():
+    print('________________Lịch thi đấu bảng G________________')
+    print('_Thuỵ Sỹ & Cameroon>>>\nTime: 17:00\nDate: 24/11/2022')
+    print('_Brazil & Serbia>>>\nTime: 02:00\nDate: 25/11/2022')
+    print('_Cameroon & Serbia>>>\nTime: 17:00\nDate: 28/11/2022')
+    print('_Brazil & Thuỵ Sỹ>>>\nTime: 23:00\nDate: 28/11/2022')
+    print('_Cameroon & Brazil>>>\nTime: 02:00\nDate: 03/12/2022')
+    print('_Serbia & Thuỵ Sỹ>>>\nTime: 02:00\nDate: 03/12/2022')
+    print('___________________________________________________')
+
+def lichthidaubangH():
+    print('________________Lịch thi đấu bảng H________________')
+    print('_Uruguay & Hàn Quốc>>>\nTime: 20:00\nDate: 24/11/2022')
+    print('_Bồ Đào Nha & Ghana>>>\nTime: 23:00\nDate: 25/11/2022')
+    print('_Hàn Quốc & Ghana>>>\nTime: 20:00\nDate: 28/11/2022')
+    print('_Bồ Đào Nha & Uruguay>>>\nTime: 02:00\nDate: 29/11/2022')
+    print('_Hàn Quốc & Bồ Đào Nha>>>\nTime: 22:00\nDate: 02/12/2022')
+    print('_Ghana & Uruguay>>>\nTime: 22:00\nDate: 02/12/2022')
+    print('___________________________________________________')
+
+def tracuuwc(c, d):
+    print(f'__________Kết quả trận đấu ngày {c}/11/2022_________')
+    if c == 20:
+        if d == 1:
+            print("Ecuador 2-0 Quatar")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 21:
+        if d == 1:
+            print("Senegal 0-2 Hà Lan ")
+        elif d == 2:
+            print("Anh 6-2 Iran ")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 22:
+        if d == 2:
+            print("Hoa Kì 1-1 Wales ")
+        elif d == 3:
+            print("Argentina 1-2 Ả rập xê út ")
+            print("Mexico 0-0 Ba lan ")
+        elif d == 4:
+            print("Đan mạch 0 -0 Tunisia ")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 23:
+        if d == 4:
+            print("Pháp 4-1 Úc ")
+        elif d == 6:
+            print("Ma-rốc 0-0 Croatia ")
+        elif d == 5:
+            print("Đức 1-2 Nhật Bản\nTây Ban Nha 7 -0 Costa Rica  ")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 24:
+        if d == 6:
+            print("Bỉ 1-0 Canada ")
+        elif d == 7:
+            print("Thụy Sĩ 1-0 Cameron ")
+        elif d == 8:
+            print("Urugay 0-0 Hàn Quốc\nBồ Đào Nha 3-2 Ghana  ")
+        else:
+            print("Không có trận đấu nào cả")
+        print('___________________________________________________')
+    if c == 25:
+        if d == 7:
+            print("Brazil 2-0 Serbia ")
+        elif d == 2:
+            print("Wales 0-2 Iran ")
+        elif d == 1:
+            print("Hà Lan 1-1 Ecuador")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 26:
+        if d == 2:
+            print("Anh 0-0 Hoa Kì ")
+        elif d == 4:
+            print("Tunisia 0-1 Úc ")
+            print("Pháp 2-1 Đan Mạch ")
+        elif d == 3:
+            print("Ba Lan 2- 0 Ả rập Xê Út ")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 27:
+        if d == 3:
+            print("Argentina 2-0 Mexico")
+        elif d == 6:
+            print("Bỉ 0-2 Maroc")
+            print("croatia 4-1 Canada")
+        elif d == 5:
+            print("nhật bản 0-1 Costa Rica")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 28:
+        if d == 5:
+            print("Tây ban nha 1-1 Đức")
+        elif d == 7:
+            print("Cameroon 3-3 Serbia")
+            print("Brasil 1-0 Thụy sĩ ")
+        elif d == 8:
+            print("Hàn quốc 2-3 Ghana")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 29:
+        if d == 8:
+            print("Bồ đào nha 2-0 Uruguay")
+        elif d == 1:
+            print("Ecuador1-2 Senegal")
+            print("Hà lan 2-0 Quatar")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+    if c == 30:
+        if d == 2:
+            print("Iran 0-1 Hoa kì")
+            print("Wales 0-3 Anh")
+        elif d == 4:
+            print("Tunisia 1-0 Pháp")
+            print("Úc 1-0 Đan Mạch")
+        else:
+            print("Không có trận đấu nào cả ")
+        print('___________________________________________________')
+
+def thongtinbangdau(a):
+    if a == 1:
+        print("Bảng A gồm 4 đội : Quatar ; Ecuador ; Senegal ; Hà Lan")
+    if a == 2:
+        print("Bảng B gồm 4 đội : Anh ; Iran ; Mỹ ; Wales")
+    if a == 3:
+        print("Bảng C gồm 4 đội : Argentina ; Saudi Arabia ; Mexico ; Ba Lan")
+    if a == 4:
+        print("Bảng D gồm 4 đội : Pháp ; Autralia ; Đan mạch ; Tunisia")
+    if a == 5:
+        print("Bảng E gồm 4 đội : Tây Ban Nha ; Costa Rica ; Đức ; Nhật Bản")
+    if a == 6:
+        print("Bảng F gồm 4 đội : Bỉ ; Canada ; Morocco ; Croatia")
+    if a == 7:
+        print("Bảng G gồm 4 đội : Brazil ; Serbia ; Thụy Sĩ ; Camerooon")
+    if a == 8:
+        print("Bảng H gồm 4 đội : Bồ Đào Nha ; Ghana ; Uruguay ; Hàn Quốc")
